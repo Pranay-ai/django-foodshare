b010670b523f221fff4a67e1fdad7f7d19ff0dc4
Pranay Guda <65746692+Pranay-ai@users.noreply.github.com>
2024-07-14 10:35:47 -0700

    Update settings.py
    Made changes to setting

--- foodshare/settings.py
+++ foodshare/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oa&vb7pnv(y@qgsgq==*f$ucbv0bq@3b%nh83#p#^i*2senmmc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["3.209.54.53"]
 
 
 # Application definition
